2d0afe407cfaa1725281999aa9963d34d9f04a00
Tarun <tarun.mohandas@gmail.com>
2018-10-09 22:58:20 +0530

    a search of ice and fire done

--- Question3/javascript/index.js
+++ Question3/javascript/index.js
@@ -55,37 +55,6 @@ var get_characters = function(page_no)
           }
           key = key.toLowerCase();
           characters[key] = c_url;
-          // if(name==="")
-          // {
-          //   aliases = character["aliases"];
-          //   for(var j=0; j<aliases.length; j++)
-          //   {
-          //     var alias = aliases[j];
-          //     alias = alias.toLowerCase();
-          //     if(alias in characters)
-          //     {
-          //       characters[alias].push(c_url);
-          //     }
-          //     else
-          //     {
-          //       characters[alias] = [];
-          //       characters[alias].push(c_url);
-          //     }
-          //   }
-          // }
-          // else
-          // {
-          //   name = name.toLowerCase();
-          //   if(name in characters)
-          //   {
-          //     characters[name].push(c_url);
-          //   }
-          //   else
-          //   {
-          //     characters[name] = [];
-          //     characters[name].push(c_url);
-          //   }
-          // }
           total_characters++;
         }
         get_characters((page_no+1));
@@ -104,50 +73,65 @@ var display_char_data = function(character_url)
     {
       var name = response["name"];
       $("#char_string").empty();
-      $("#char_string").append(name);
-      $("#char_string_div").show();
+      if(name!=="")
+      {
+        $("#char_string").append(name);
+        $("#char_string_div").show();
+      }
       var gender = response["gender"];
       $("#char_gender").empty();
-      $("#char_gender").append(gender);
-      $("#char_gender_div").show();
+      if(gender!=="")
+      {
+        $("#char_gender").append(gender);
+        $("#char_gender_div").show();
+      }
       var culture = response["culture"];
       $("#char_culture").empty();
-      $("#char_culture").append(culture);
-      $("#char_culture_div").show();
+      if(culture!=="")
+      {
+        $("#char_culture").append(culture);
+        $("#char_culture_div").show();
+      }
       var born = response["born"];
       $("#char_born").empty();
-      $("#char_born").append(born);
-      $("#char_born_div").show();
+      if(born!=="")
+      {
+        $("#char_born").append(born);
+        $("#char_born_div").show();
+      }
       var died = response["died"];
       $("#char_died").empty();
-      $("#char_died").append(died);
-      $("#char_died_div").show();
+      if(died!=="")
+      {
+        $("#char_died").append(died);
+        $("#char_died_div").show();
+      }
       var titles = response["titles"];
       $("#char_titles").empty();
       for(var i=0; i<titles.length; i++)
       {
-        if(i==0)
+        if(i==0 && titles[i]!=="")
         {
           $("#char_titles_div").show();
         }
-        $("#char_titles").append(titles[i]);
-        if(i!=(titles.length-1))
+        if(titles[i]!=="")
         {
-          $("#char_titless").append(",");
+          $("#char_titles").append(titles[i]);
+          $("#char_titles").append(" * ");
         }
       }
       var aliases = response["aliases"];
       $("#char_aliases").empty();
       for(var i=0; i<aliases.length; i++)
       {
-        if(i==0)
+        if(i==0 && aliases[i]!=="")
         {
           $("#char_aliases_div").show();
         }
-        $("#char_aliases").append(aliases[i]);
-        if(i!=(aliases.length-1))
+        if(aliases[i]!=="")
         {
-          $("#char_aliases").append(",");
+          $("#char_aliases").append(aliases[i]);
+          $("#char_aliases").append(" * ");
         }
       }
       var allegiances = response["allegiances"];
@@ -165,10 +149,7 @@ var display_char_data = function(character_url)
           success: function(response)
           {
             $("#char_allegiances").append(response["name"]);
-            if(i!=(allegiances.length-1))
-            {
-              $("#char_allegiances").append(",");
-            }
+            $("#char_allegiances").append(" * ");
           }
         });
       }
@@ -176,9 +157,8 @@ var display_char_data = function(character_url)
   });
 }
 
-var display_house_data = function(house_full_name)
+var display_house_data = function(house_url)
 {
-  var house_url = houses[house_full_name];
   $("#search_urls").empty();
   $.ajax({
   url: house_url,
@@ -187,46 +167,58 @@ var display_house_data = function(house_full_name)
   {
     var name = response["name"];
     $("#house_string").empty();
-    $("#house_string").append(name);
-    $("#house_string_div").show();
+    if(name!=="")
+    {
+      $("#house_string").append(name);
+      $("#house_string_div").show();
+    }
     var region = response["region"];
     $("#house_region").empty();
-    $("#house_region").append(region);
-    $("#house_region_div").show();
+    if(region!=="")
+    {
+      $("#house_region").append(region);
+      $("#house_region_div").show();
+    }
     var coatOfArms = response["coatOfArms"];
     $("#house_sigil").empty();
-    $("#house_sigil").append(coatOfArms);
-    $("#house_sigil_div").show();
+    if(coatOfArms!=="")
+    {
+      $("#house_sigil").append(coatOfArms);
+      $("#house_sigil_div").show();
+    }
     var words = response["words"];
     $("#house_words").empty();
-    $("#house_words").append(words);
-    $("#house_words_div").show();
+    if(words!=="")
+    {
+      $("#house_words").append(words);
+      $("#house_words_div").show();
+    }
     var titles = response["titles"];
     $("#house_titles").empty();
     for(var i=0; i<titles.length; i++)
     {
-      if(i==0)
+      if(i==0 && titles[0]!=="")
       {
         $("#house_titles_div").show();
       }
-      $("#house_titles").append(titles[i]);
-      if(i!=(titles.length-1))
+      if(titles[i]!=="")
       {
-        $("#house_titles").append(",");
+        $("#house_titles").append(titles[i]);
+        $("#house_titles").append(" * ");
       }
     }
     var seats = response["seats"];
     $("#house_seats").empty();
     for(var i=0; i<seats.length; i++)
     {
-      if(i==0)
+      if(i==0 && seats[i]!=="")
       {
         $("#house_seats_div").show();
-      }
-      $("#house_seats").append(seats[i]);
-      if(i!=(seats.length-1))
+      };
+      if(seats[i]!=="")
       {
-        $("#house_seats").append(",");
+        $("#house_seats").append(seats[i]);
+        $("#house_seats").append(" * ");
       }
     }
     var lord_url = response["currentLord"];
@@ -236,8 +228,12 @@ var display_house_data = function(house_full_name)
       dataType: "json",
       success: function(response)
       {
-        $("#house_lord_div").show();
-        $("#house_lord").append(response["name"]);
+        var lord_name = response["name"];
+        if(lord_name!=="")
+        {
+          $("#house_lord_div").show();
+          $("#house_lord").append(response["name"]);
+        }
       }
     });
     var heir_url = response["heir"];
@@ -247,8 +243,12 @@ var display_house_data = function(house_full_name)
       dataType: "json",
       success: function(response)
       {
-        $("#house_heir_div").show();
-        $("#house_heir").append(response["name"]);
+        var heir_name = response["name"];
+        if(heir!=="")
+        {
+          $("#house_heir_div").show();
+          $("#house_heir").append(response["name"]);
+        }
       }
     });
     var overlord_url = response["overlord"];
@@ -258,14 +258,21 @@ var display_house_data = function(house_full_name)
       dataType: "json",
       success: function(response)
       {
-        $("#house_overlord_div").show();
-        $("#house_overlord").append(response["name"]);
+        var lord_name = response["name"];
+        if(lord_name!=="")
+        {
+          $("#house_overlord_div").show();
+          $("#house_overlord").append(response["name"]);
+        }
       }
     });
     var founded = response["founded"];
     $("#house_founded").empty();
-    $("#house_founded").append(founded);
-    $("#house_founded_div").show();
+    if(found!=="")
+    {
+      $("#house_founded").append(founded);
+      $("#house_founded_div").show();
+    }
     var founder_url = response["founder"];
     $("#house_founder").empty();
     $.ajax({
@@ -273,22 +280,26 @@ var display_house_data = function(house_full_name)
       dataType: "json",
       success: function(response)
       {
-        $("#house_founder_div").show();
-        $("#house_founder").append(response["name"]);
+        var found_name = response["name"];
+        if(found_name!=="")
+        {
+          $("#house_founder_div").show();
+          $("#house_founder").append(response["name"]);
+        }
       }
     });
     var weapons = response["ancestralWeapons"];
     $("#house_weapon").empty();
     for(var i=0; i<weapons.length; i++)
     {
-      if(i==0)
+      if(i==0 && weapons[i]!=="")
       {
         $("#house_weapon_div").show();
       }
-      $("#house_weapon").append(weapons[i]);
-      if(i!=(weapons.length-1))
+      if(weapons[i]!=="")
       {
-        $("#house_weapon").append(",");
+        $("#house_weapon").append(weapons[i]);
+        $("#house_weapon").append(" * ");
       }
     }
     var cadetBranches = response["cadetBranches"];
@@ -306,10 +317,7 @@ var display_house_data = function(house_full_name)
         success: function(response)
         {
           $("#house_cadet").append(response["name"]);
-          if(i!=(cadetBranches.length-1))
-          {
-            $("#house_cadet").append(",");
-          }
+          $("#house_cadet").append(" * ");
         }
       });
     }
@@ -328,17 +336,48 @@ var display_house_data = function(house_full_name)
         success: function(response)
         {
           $("#house_sworn").append(response["name"]);
-          if(i!=(swornMembers.length-1))
-          {
-            $("#house_sworn").append(",");
-          }
+          $("#house_sworn").append(" * ");
         }
       });
     }
-
   }
   });
 
+}
+
+var get_characters_from_house = function(house_name)
+{
+  var character_names = [];
+  var house_split = house_name.split(" ");
+  for(var i=0; i<house_split.length; i++)
+  {
+    var house_name = house_split[i];
+    if(house_name==="house")
+    {
+      continue;
+    }
+    for(char_name in characters)
+    {
+      if(!characters.hasOwnProperty(char_name))
+      {
+        continue;
+      }
+      var char_name_split = char_name.split(" ");
+      for(var j=0; j<char_name_split.length; j++)
+      {
+        var sur_name = char_name_split[j];
+        if(sur_name===house_name)
+        {
+          character_names.push(char_name);
+        }
+      }
+    }
+    if(character_names.length>0)
+    {
+      break;
+    }
+  }
+  return character_names;
 }
 
 
@@ -358,10 +397,48 @@ $(document).ready(function(){
           $("div.search_div").hide();
           $("#house_div").show();
         }
-        else
+        else if(btn_value==="search_character")
         {
           $("div.search_div").hide();
           $("#char_div").show();
+        }
+        else
+        {
+          $("div.search_div").hide();
+          $("#house_char_div").show();
+        }
+    });
+
+    $("#rand_house").click(function(){
+        $("div.char_display").hide();
+        $("div.search_div").hide();
+        var rh = Math.floor((Math.random() * total_houses) + 1);
+        var h_url = "https://anapioficeandfire.com/api/houses/"+rh;
+        display_house_data(h_url);
+    });
+    $("#rand_char").click(function(){
+        $("div.house_display").hide();
+        $("div.search_div").hide();
+        var rc = Math.floor((Math.random() * total_characters) + 1);
+        var c_url = "https://anapioficeandfire.com/api/characters/"+rc;
+        display_char_data(c_url);
+    });
+
+    $("#search_house_char").click(function(){
+        $("div.char_display").hide();
+        var house_name = $("#house_char_name").val();
+        var char_names = get_characters_from_house(house_name);
+        if(char_names.length==0)
+        {
+          $("#got_char_display").empty();
+          $("#got_char_display").append("<p class='text'>No results found</p>");
+        }
+        else
+        {
+          var rc = Math.floor((Math.random() * (char_names.length-1)));
+          var char_name = char_names[rc];
+          var char_url = characters[char_name];
+          display_char_data(char_url);
         }
     });
 
@@ -370,22 +447,23 @@ $(document).ready(function(){
     $("div.char_display").hide();
     var char_name = $("#char_name").val();
     char_name = char_name.toLowerCase();
-    console.log("char_name received=="+char_name);
+    var count = 0;
     for(ch in characters)
     {
-      console.log("ch=="+ch);
       if(!characters.hasOwnProperty(ch))
       {
         continue;
       }
       if(ch.indexOf(char_name)>-1)
       {
+        count++;
         var su = document.getElementById("search_urls");
         var div_node = document.createElement("div");
         div_node.setAttribute("class","row");
         var btn = document.createElement("button");
         btn.setAttribute("class","search_result_button");
         var char_url = characters[ch];
+        temp_url = char_url;
         var func_str = "display_char_data('"+char_url+"')";
         btn.setAttribute("onclick",func_str);
         var buttonText = document.createTextNode(ch);
@@ -394,12 +472,21 @@ $(document).ready(function(){
         su.appendChild(div_node);
       }
     }
-
+    if(count==0)
+    {
+      $("#got_char_display").empty();
+      $("#got_char_display").append("<p class='text'>No results found</p>");
+    }
+    if(count==1)
+    {
+      display_char_data(temp_url);
+    }
   });
   $("#search_house").click(function(){
     $("div.house_display").hide();
     var house_name = $("#house_name").val();
     house_name = house_name.toLowerCase();
+    var count = 0;
     for(house in houses)
     {
       if(!houses.hasOwnProperty(house))
@@ -408,12 +495,15 @@ $(document).ready(function(){
       }
       if(house.indexOf(house_name)>-1)
       {
+        count++;
         var su = document.getElementById("search_urls");
         var div_node = document.createElement("div");
         div_node.setAttribute("class","row");
         var btn = document.createElement("button");
         btn.setAttribute("class","search_result_button");
-        var func_str = "display_house_data('"+house+"')";
+        var house_url = houses[house];
+        temp_url = house_url;
+        var func_str = "display_house_data('"+house_url+"')";
         btn.setAttribute("onclick",func_str);
         var buttonText = document.createTextNode(house);
         btn.appendChild(buttonText);
@@ -421,7 +511,14 @@ $(document).ready(function(){
         su.appendChild(div_node);
       }
     }
-
+    if(count==0)
+    {
+      $("#got_house_display").empty();
+      $("#got_house_display").append("<p class='text'>No results found</p>");
+    }
+    else if(count==1)
+    {
+      display_house_data(temp_url);
+    }
   });
-
 });
