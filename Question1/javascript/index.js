b5188499f7b24ad9921c96a8ea1ea43c0917ee89
Tarun <tarun.mohandas@gmail.com>
2018-10-09 23:34:34 +0530

    added clear buttons to que1

--- Question1/javascript/index.js
+++ Question1/javascript/index.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-  $("#mybutton").click(function(){
+  $("#try_it").click(function(){
     var text = $("#myTextarea").val();
-    //console.log(text);
     document.getElementById('myIframe').contentDocument.write(text);
+  });
+  $("#clear_code").click(function(){
+    $("#myTextarea").val('');
+  });
+  $("#clear_display").click(function(){
+    $("#myIframe").attr("src","about:blank");
   });
 });
